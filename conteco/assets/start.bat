3fe50d5d62f5a4d57e46cee52fe45b487add0956
hvb <herman@vcweb.co>
2020-07-10 18:39:38 +0000

    cli refactor, repository categories & shortened commands

---
+++ conteco/assets/start.bat
@@ -0,0 +1,4 @@
+@echo off
+
+echo "Starting controlplane.${CONTECO_STARTUP_MODE}"
+docker run -it -v /etc:/hostfs/etc:ro -v /var/run/docker.sock:/var/run/docker.sock -v controlplane_repos_${CONTECO_REPOSITORY_CATEGORY}:/conteco/pwd ${CONTECO_REGISTRY}${CONTECO_REALM}/${CONTECO_ECOSYSTEM}.controlplane.${CONTECO_STARTUP_MODE} --interactive start windows ${CONTECO_REPOSITORY_CATEGORY} %1
