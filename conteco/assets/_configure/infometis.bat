bde9df5be0e4001c720426cc9716289f3da075e4
hvb <herman@vcweb.co>
2020-07-27 18:29:58 +0000

    removed Windows .bat file

--- conteco/assets/_configure/infometis.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-#echo "Configuring InfoMetis ..."
-#docker run -it -v /etc:/hostfs/etc:ro -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/conteco/pwd ${CONTECO_REGISTRY}${CONTECO_REALM}/${CONTECO_ECOSYSTEM}.controlplane.cli configure-infometis %*
